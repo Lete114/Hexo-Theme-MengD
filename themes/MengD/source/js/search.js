15c355252800635c3c4ed20b4b07acf812f342e3
lete114 <lete@lete114.top>
2021-04-28 16:52:12 +0800

    调整: Search的样式 修复: Search搜索时出现严重 卡顿、在开启了pjax功能后搜索出结果，并进入搜索结果 的页面后重新打开搜索框时不显示的bug、优化代码实现

--- themes/MengD/source/js/search.js
+++ themes/MengD/source/js/search.js
@@ -1,4 +1,3 @@
-
 var local_search = document.getElementById("local-search");
 var html = document.querySelector("html")
 var search_mask = document.getElementById("search-mask")
@@ -15,102 +14,77 @@ var searchFunc = function (path, search_id, content_id) {
                     url: item.querySelector('url').textContent
                 }
             })
-            datas.forEach(function (item) {
-                var $input = document.getElementById(search_id);
-                if (!$input) return;
-                var $resultContent = document.getElementById(content_id);
-                $input.addEventListener('input', function () {
-                    var str = '<ul class=\"search-result-list\">';
-                    var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-                    $resultContent.innerHTML = "";
-                    if (this.value.trim().length <= 0) {
-                        return;
-                    }
-                    // perform local searching
-                    datas.forEach(function (data) {
-                        var isMatch = true;
-                        var content_index = [];
-                        if (!data.title || data.title.trim() === '') {
-                            data.title = "Untitled";
-                        }
-                        var data_title = data.title.trim().toLowerCase();
-                        var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
-                        var data_url = data.url.startsWith('/') ? data.url : "/" + data.url;
-                        var index_title = -1;
-                        var index_content = -1;
-                        var first_occur = -1;
-                        // only match artiles with not empty contents
-                        if (data_content !== '') {
-                            keywords.forEach(function (keyword, i) {
-                                index_title = data_title.indexOf(keyword);
-                                index_content = data_content.indexOf(keyword);
+            var $input = document.getElementById(search_id);
+            var $resultContent = document.getElementById(content_id);
+            $input.addEventListener('input', function () {
+                var str = '<ul class=\"search-result-list\">';
+                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                $resultContent.innerHTML = "";
+                if (this.value.trim().length <= 0) return;
+                // perform local searching
+                datas.forEach(function (data) {
+                    var isMatch = true;
+                    if (!data.title || data.title.trim() === '') data.title = "Untitled";
+                    var data_title = data.title.trim().toLowerCase();
+                    var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
+                    var data_url = data.url.startsWith('/') ? data.url : "/" + data.url;
+                    var index_title = -1;
+                    var index_content = -1;
+                    var first_occur = -1;
+                    // only match artiles with not empty contents
+                    if (data_content !== '') {
+                        keywords.forEach(function (keyword, i) {
+                            index_title = data_title.indexOf(keyword);
+                            index_content = data_content.indexOf(keyword);
 
-                                if (index_title < 0 && index_content < 0) {
-                                    isMatch = false;
-                                } else {
-                                    if (index_content < 0) {
-                                        index_content = 0;
-                                    }
-                                    if (i == 0) {
-                                        first_occur = index_content;
-                                    }
-                                    // content_index.push({index_content:index_content, keyword_len:keyword_len});
-                                }
-                            });
-                        } else {
-                            isMatch = false;
-                        }
-                        // show search results
-                        if (isMatch) {
-                            var content = data.content.trim().replace(/<[^>]+>/g, "");
-                            if (first_occur >= 0) {
-                                // cut out 100 characters
-                                var start = first_occur - 20;
-                                var end = first_occur + 80;
-
-                                if (start < 0) {
-                                    start = 0;
-                                }
-
-                                if (start == 0) {
-                                    end = 100;
-                                }
-
-                                if (end > content.length) {
-                                    end = content.length;
-                                }
-
-                                var match_content = content.substring(start, end);
-
-                                // highlight all keywords
-                                keywords.forEach(function (keyword) {
-                                    var regS = new RegExp(keyword, "gi");
-                                    match_content = match_content.replace(regS, "<span class=\"search-keyword\">" + keyword + "</span>");
-                                    data_title = data_title.replace(regS, "<span class=\"search-keyword\">" + keyword + "</span>");
-                                });
-                                str += "<li><a href='" + data_url + "' class='search-result-title'>" + data_title + "</a>";
-                                str += "<p class=\"search-result\">" + match_content + "...</p>"
+                            if (index_title < 0 && index_content < 0) isMatch = false;
+                            else {
+                                if (index_content < 0) index_content = 0;
+                                if (i == 0) first_occur = index_content;
                             }
-                            str += "</li>";
+                        });
+                    } else isMatch = false;
+                    // show search results
+                    if (isMatch) {
+                        var content = data.content.trim().replace(/<[^>]+>/g, "");
+                        if (first_occur >= 0) {
+                            // cut out 100 characters
+                            var start = first_occur - 20;
+                            var end = first_occur + 80;
+                            if (start < 0) start = 0;
+                            if (start == 0) end = 100;
+                            if (end > content.length) end = content.length;
+                            var match_content = content.substring(start, end);
+
+                            // highlight all keywords
+                            keywords.forEach(function (keyword) {
+                                var regS = new RegExp(keyword, "gi");
+                                match_content = match_content.replace(regS, "<span class=\"search-keyword\">" + keyword + "</span>");
+                                data_title = data_title.replace(regS, "<span class=\"search-keyword\">" + keyword + "</span>");
+                            });
+                            str += "<li><a href='" + data_url + "' class='search-result-title'>" + data_title + "</a>";
+                            str += "<p class=\"search-result\">" + match_content + "...</p>"
                         }
-                    });
-                    str += "</ul>";
-                    $resultContent.innerHTML = str;
-                    window.pjax && window.pjax.refresh($resultContent)
+                        str += "</li>";
+                    }
                 });
-            })
+                str += "</ul>";
+                $resultContent.innerHTML = str;
+                window.pjax && window.pjax.refresh($resultContent)
+            });
+            // })
         })
 }
 searchFunc($config.search.path, 'local-search-input', 'local-search-result')
 
 
 // 显示搜索框
-document.querySelectorAll(".search-btn").forEach(function(item){
-    item.onclick=function(){
-        search_mask.className="mask"
-        if(local_search.style.display=="none"){
-            local_search.style.display="block"
-            html.style.overflow="hidden";
+document.querySelectorAll(".search-btn").forEach(function (item) {
+    item.onclick = function () {
+        search_mask.className = "mask"
+        if (local_search.style.display == "none") {
+            local_search.style.display = "block"
+            html.style.overflow = "hidden";
             local_search.classList.remove("search-animation-min")
             local_search.classList.add("search-animation-max")
             document.getElementById("local-search-input").focus()
@@ -119,28 +93,28 @@ document.querySelectorAll(".search-btn").forEach(function(item){
 })
 
 // 关闭搜索框
-document.querySelector(".search-close-button").onclick=function(){
+document.querySelector(".search-close-button").onclick = function () {
     local_search.classList.remove("search-animation-max")
     local_search.classList.add("search-animation-min")
     search_mask.classList.remove("mask")
-    html.style.overflow="auto";
-    setTimeout(function(){
-        local_search.style.display="none"
-    },500)
+    html.style.overflow = "auto";
+    setTimeout(function () {
+        local_search.style.display = "none"
+    }, 500)
 }
 
 
 // 关闭所有正在打开的弹窗
-search_mask.onclick=function(){
+search_mask.onclick = function () {
     local_search.classList.remove("search-animation-max")
     local_search.classList.add("search-animation-min")
-    html.style.overflow="auto";
-    search_mask.className=""
-    setTimeout(function(){
-        local_search.style.display="none"
-    },500)
+    html.style.overflow = "auto";
+    search_mask.className = ""
+    setTimeout(function () {
+        local_search.style.display = "none"
+    }, 500)
 }
 
 window.addEventListener('pjax:complete', function () {
-    local_search.style.display === 'none' ? search_mask.className="":""
+    local_search.style.display === 'none' ? search_mask.className = "" : ""
 })
