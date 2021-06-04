c87ea39091034833d238459c87c3584f8a37f981
lete114 <lete@lete114.top>
2021-06-04 15:09:06 +0800

    修改: 当开启图片懒加载时点击【直达评论】时出现直达评论未能滚动到评论区域

--- source/js/lazyLoad.js
+++ source/js/lazyLoad.js
@@ -3,22 +3,24 @@
  * @param {*} img 需要懒加载的img元素(标签)
  * @param {*} attr 图片的真实url地址
  */
-function ImgLazyLoad(img,attr){
+function ImgLazyLoad(img, attr) {
     var imgLazyLoad = document.querySelectorAll(img)
-    function LazyLoad(target){
-        const io = new IntersectionObserver((entries,Observer)=>{
-            entries.forEach(entry=>{
-                if(entry.isIntersecting){
-                    const img = entry.target
-                    const src = img.getAttribute(attr)
-                    img.setAttribute("src",src)
-                    Observer.disconnect()
-                }   
+    function LazyLoad(target) {
+        const io = new IntersectionObserver((entries, Observer) => {
+            entries.forEach(entry => {
+                setTimeout(function () {
+                    if (entry.isIntersecting) {
+                        const img = entry.target
+                        const src = img.getAttribute(attr)
+                        img.setAttribute("src", src)
+                        Observer.disconnect()
+                    }
+                }, 500)
             })
         })
         io.observe(target)
     }
     imgLazyLoad.forEach(LazyLoad)
 }
-ImgLazyLoad("body img[data-img]","data-img")
+ImgLazyLoad("body img[data-img]", "data-img")
 
