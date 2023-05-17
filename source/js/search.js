e9dd38cdd66d35f615382aa1641e0c0076bea8e8
Lete114 <me@imlete.cn>
2023-05-17 22:36:45 +0800

    feat: search minify

--- source/js/search.js
+++ source/js/search.js
@@ -38,7 +38,10 @@
   history_list.appendChild(history_list_cdf)
 
   const response = await fetch(path)
-  let datas = await response.json()
+  const datas = (await response.json()).map((item) => {
+    item.content = ZLStringDecompress(item.content)
+    return item
+  })
 
   input.addEventListener('input', () => {
     const value = input.value.trim()
@@ -98,7 +101,7 @@
           // highlight all keywords
           keywords.forEach((keyword) => {
             const reg = new RegExp(`(${keyword})`, 'gi')
-            const key ='<span class="search-keyword">$1</span>'
+            const key = '<span class="search-keyword">$1</span>'
             matchContent = matchContent.replace(reg, key)
             dataTitle = dataTitle.replace(reg, key)
           })
@@ -127,4 +130,171 @@
 
     result.appendChild(cdf)
   })
+
+  function ZLStringDecompress(compressed) {
+    const f = String.fromCharCode
+    const length = compressed.length
+    const resetValue = 32768
+    if (!compressed) return ''
+    var dictionary = [],
+      enlargeIn = 4,
+      dictSize = 4,
+      numBits = 3,
+      entry = '',
+      result = [],
+      i,
+      w,
+      bits,
+      resb,
+      maxpower,
+      power,
+      c,
+      data = { val: compressed.charCodeAt(0), position: resetValue, index: 1 }
+
+    for (i = 0; i < 3; i += 1) {
+      dictionary[i] = i
+    }
+
+    bits = 0
+    maxpower = Math.pow(2, 2)
+    power = 1
+    while (power != maxpower) {
+      resb = data.val & data.position
+      data.position >>= 1
+      if (data.position == 0) {
+        data.position = resetValue
+        data.val = compressed.charCodeAt(data.index++)
+      }
+      bits |= (resb > 0 ? 1 : 0) * power
+      power <<= 1
+    }
+
+    switch ((next = bits)) {
+      case 0:
+        bits = 0
+        maxpower = Math.pow(2, 8)
+        power = 1
+        while (power != maxpower) {
+          resb = data.val & data.position
+          data.position >>= 1
+          if (data.position == 0) {
+            data.position = resetValue
+            data.val = compressed.charCodeAt(data.index++)
+          }
+          bits |= (resb > 0 ? 1 : 0) * power
+          power <<= 1
+        }
+        c = f(bits)
+        break
+      case 1:
+        bits = 0
+        maxpower = Math.pow(2, 16)
+        power = 1
+        while (power != maxpower) {
+          resb = data.val & data.position
+          data.position >>= 1
+          if (data.position == 0) {
+            data.position = resetValue
+            data.val = compressed.charCodeAt(data.index++)
+          }
+          bits |= (resb > 0 ? 1 : 0) * power
+          power <<= 1
+        }
+        c = f(bits)
+        break
+      case 2:
+        return ''
+    }
+    dictionary[3] = c
+    w = c
+    result.push(c)
+    while (true) {
+      if (data.index > length) {
+        return ''
+      }
+
+      bits = 0
+      maxpower = Math.pow(2, numBits)
+      power = 1
+      while (power != maxpower) {
+        resb = data.val & data.position
+        data.position >>= 1
+        if (data.position == 0) {
+          data.position = resetValue
+          data.val = compressed.charCodeAt(data.index++)
+        }
+        bits |= (resb > 0 ? 1 : 0) * power
+        power <<= 1
+      }
+
+      switch ((c = bits)) {
+        case 0:
+          bits = 0
+          maxpower = Math.pow(2, 8)
+          power = 1
+          while (power != maxpower) {
+            resb = data.val & data.position
+            data.position >>= 1
+            if (data.position == 0) {
+              data.position = resetValue
+              data.val = compressed.charCodeAt(data.index++)
+            }
+            bits |= (resb > 0 ? 1 : 0) * power
+            power <<= 1
+          }
+
+          dictionary[dictSize++] = f(bits)
+          c = dictSize - 1
+          enlargeIn--
+          break
+        case 1:
+          bits = 0
+          maxpower = Math.pow(2, 16)
+          power = 1
+          while (power != maxpower) {
+            resb = data.val & data.position
+            data.position >>= 1
+            if (data.position == 0) {
+              data.position = resetValue
+              data.val = compressed.charCodeAt(data.index++)
+            }
+            bits |= (resb > 0 ? 1 : 0) * power
+            power <<= 1
+          }
+          dictionary[dictSize++] = f(bits)
+          c = dictSize - 1
+          enlargeIn--
+          break
+        case 2:
+          return result.join('')
+      }
+
+      if (enlargeIn == 0) {
+        enlargeIn = Math.pow(2, numBits)
+        numBits++
+      }
+
+      if (dictionary[c]) {
+        entry = dictionary[c]
+      } else {
+        if (c === dictSize) {
+          entry = w + w.charAt(0)
+        } else {
+          return null
+        }
+      }
+      result.push(entry)
+
+      // Add w+entry[0] to the dictionary.
+      dictionary[dictSize++] = w + entry.charAt(0)
+      enlargeIn--
+
+      w = entry
+
+      if (enlargeIn == 0) {
+        enlargeIn = Math.pow(2, numBits)
+        numBits++
+      }
+    }
+  }
 })()
